5e3ef3bdcaee47fc897922b3c811d16e922ccb6a
alfredocaso999-tech <alfredocaso999@gmail.com>
2026-07-10 03:16:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,15 @@ PRODOTTI = {
         "prezzo": "2g 10€ 5g 20€",
         "video_url": "https://res.cloudinary.com/dg1axjftz/video/upload/v1779706002/5_tvcxfq.mp4"
     },
-    "2": {
+    "8": {
         "nome": "🍫DRY FRESH🍫",
         "prezzo": "1g 10€ 5g 30€",
         "video_url": "https://res.cloudinary.com/dg1axjftz/video/upload/v1783451780/2_n4hduq.mp4"
+    },
+    "16": {
+        "nome": "🍫DRY OREO PANCAKES🍫",
+        "prezzo": "1g 10€ 5g 35€",
+        "video_url": "https://res.cloudinary.com/dg1axjftz/video/upload/v1783646156/7_oja1rm.mp4"
     },
     "2": {
         "nome": "🍫DRY KINDER BUFALO PLEIN🍫",
