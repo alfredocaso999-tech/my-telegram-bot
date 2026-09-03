0147460e8e33785a40ae2baf7175cfdfdc5d7d90
alfredocaso999-tech <alfredocaso999@gmail.com>
2026-09-03 04:08:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ PRODOTTI = {
 USERNAME_VENDITORE = "the_true_freedom"
 
 # ==================== URL DELL'IMMAGINE ====================
-URL_IMMAGINE = "https://i.postimg.cc/1XrsqwcH/5803207322296326395-119.png"
+URL_IMMAGINE = "https://i.postimg.cc/kDrJmWfN/5821090887512035322-119.jpg"
 
 # ==================== COMANDO START ====================
 @bot.message_handler(commands=['start'])
